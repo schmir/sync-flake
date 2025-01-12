9764e6994780a9afcd90e0967130a396514f07d6
Ralf Schmitt <ralf@systemexit.de>
2025-01-12 23:42:17 +0100

    Track home-manager

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = _: { };
